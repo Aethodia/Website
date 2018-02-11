808ea1b4d95b9c31f4013ce9911bdc5fd5370575
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2018-02-11 01:01:28 -0500

    Wrote a script that pulls files from other projects

--- update-deps.bash
+++ update-deps.bash
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+
+## Clone latest
+cd build
+git clone https://github.com/h5bp/html5-boilerplate.git
+
+## Copy files
+cd html5-boilerplate
+cp -rfv .gitattributes ../../
+
+## Cleanup
+cd ..
+rm -rf html5-boilerplate

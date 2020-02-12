39217ff131a84f26becb8dc4ab48ec749460ab07
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-02-11 23:31:31 -0500

    Switched from interface to type for AnyObject

--- frontend/src/app/shared/types/technical.d.ts
+++ frontend/src/app/shared/types/technical.d.ts
@@ -1,5 +1,5 @@
 //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
 /** An object with any keys.  Discouraged;  please try to type things more-strictly wherever possible. */
-interface AnyObj extends Object {
-    [key: string]: any;
+type AnyObject = {
+    [key: string]: any,
 }

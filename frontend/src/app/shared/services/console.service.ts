05abfc5bf85c16c4d61aaed96a4162681e1b5564
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-02-13 01:13:14 -0500

    ConsoleService now manages `console` instead of wrapping it.

--- frontend/src/app/shared/services/console.service.ts
+++ frontend/src/app/shared/services/console.service.ts
@@ -4,34 +4,32 @@ import {UtilityCode} from '../misc/utility-code';
 
 ////////////////////////////////////////////////////////////////////////////////
 @Injectable()
-/** Wraps the console object.  Primary purpose is to disable logging in prod. */
-class ConsoleService extends Console {
+/** Manages the `console` object.  Primary purpose is to disable logging in prod. */
+class ConsoleService {
 
     //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
     constructor(
         private readonly varSvc: VariableService
     ) {
-        // Make `ConsoleService` identical to `console`.
-        super();
 
         // If not dev mode, replace all the properties.
         this.varSvc.getVar('isDevMode').subscribe(
             (isDevMode: boolean): void => {
                 if(!isDevMode) {
-                    for(const key in this) {
-                        if(this.hasOwnProperty(key)) {
+                    for(const key in console) {
+                        if(console.hasOwnProperty(key)) {
                             switch(key) {
 
                                 // Remove parameters from functions we want to keep
                                 case 'warn':
                                 case 'error':
                                     //@ts-ignore (We're essentially setting these to themselves -- this should always be safe.)
-                                    this[key] = (message?: any, ...optionalParams: any[]): void => console[key]()
+                                    console[key] = (message?: any, ...optionalParams: any[]): void => console[key]()
                                     break;
 
                                 // Wipe functions we do not want to keep
                                 default:
-                                    this[key] = UtilityCode.new(this[key]);
+                                    console[key] = UtilityCode.new(console[key]);
                             }
                         }
                     }

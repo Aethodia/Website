534404f67268418466a16ed60514bb71b3498151
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-02-13 01:43:47 -0500

    ConsoleService now backs up and restores `console` per `isDevMode`

--- frontend/src/app/shared/services/console.service.ts
+++ frontend/src/app/shared/services/console.service.ts
@@ -1,21 +1,34 @@
 import {Injectable} from '@angular/core';
 import {VariableService} from './variable.service';
 import {UtilityCode} from '../misc/utility-code';
+import * as _ from 'lodash';
 
 ////////////////////////////////////////////////////////////////////////////////
 @Injectable()
 /** Manages the `console` object.  Primary purpose is to disable logging in prod. */
 class ConsoleService {
 
+    /** A backup of `console`. */
+    private readonly console: Console;
+
     //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
     constructor(
         private readonly varSvc: VariableService
     ) {
+        // Back up the original console
+        this.console = _.cloneDeep(console);
 
-        // If not dev mode, replace all the properties.
+        // Figure out whether we're in dev mode
         this.varSvc.getVar('isDevMode').subscribe(
             (isDevMode: boolean): void => {
-                if(!isDevMode) {
+
+                // If dev mode, restore console
+                if(isDevMode) {
+                    console = _.cloneDeep(this.console);
+                }
+
+                // If not dev mode, gut console
+                else {
                     for(const key in console) {
                         if(console.hasOwnProperty(key)) {
                             switch(key) {
@@ -23,7 +36,6 @@ class ConsoleService {
                                 // Remove parameters from functions we want to keep
                                 case 'warn':
                                 case 'error':
-                                    //@ts-ignore (We're essentially setting these to themselves -- this should always be safe.)
                                     console[key] = (message?: any, ...optionalParams: any[]): void => console[key]()
                                     break;
 

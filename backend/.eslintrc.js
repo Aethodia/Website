8b2c604eef91a8af6be919b894a717f549ffbcca
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-02-23 15:20:05 -0500

    Fixes for eslintrcs

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
     extends: ['../shared/.eslintrc.js'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        project: './tsconfig.json',
+        tsconfigRootDir: '.',
+        project: './tsconfig.build.json',
+        createDefaultProgram: true, //FIXME:  Workaround for parserOptions.project not working correctly (https://github.com/typescript-eslint/typescript-eslint/issues/864#issuecomment-523213273)
         ecmaVersion: '2020',
         sourceType: 'module',
         ecmaFeatures: {
--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,16 +1,20 @@
 'use strict';
 module.exports = {
     extends: ['../shared/.eslintrc.js'],
+
+    plugins: ['@typescript-eslint'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        project: './tsconfig.json',
+        tsconfigRootDir: '.',
+        project: './tsconfig.app.json',
+        createDefaultProgram: true, //FIXME:  Workaround for parserOptions.project not working correctly (https://github.com/typescript-eslint/typescript-eslint/issues/864#issuecomment-523213273)
         ecmaVersion: '2020',
         sourceType: 'module',
         ecmaFeatures: {
             impliedStrict: true,
         },
     },
-    plugins: ['@typescript-eslint'],
+
     env: {
         'browser': true,
         'es6': true,
@@ -21,7 +25,6 @@ module.exports = {
     overrides: [
         {   ////////////////////////////////////////////////////////////////////////////////
             files: ['*.ts'],
-            parser: '@typescript-eslint/parser',
             plugins: [
                 '@typescript-eslint',
                 '@angular-eslint',
@@ -30,10 +33,8 @@ module.exports = {
         },
         {   ////////////////////////////////////////////////////////////////////////////////
             files: ['*.component.html'],
+            plugins: ['@angular-eslint/template',],
             parser: '@angular-eslint/template-parser',
-            plugins: [
-                '@angular-eslint/template',
-            ],
             rules: {},
         },
     ],

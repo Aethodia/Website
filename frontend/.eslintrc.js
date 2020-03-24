97fbe8d6b382fe804c3166f84bf0d0b6918cba82
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-03-24 02:22:24 -0400

    De-dotified some dotfiles

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
         tsconfigRootDir: '.',
-        project: '.tsconfig.jsonc',
+        project: 'tsconfig.json',
         createDefaultProgram: true, //BUG:  Workaround for parserOptions.project not working correctly in editors (https://github.com/typescript-eslint/typescript-eslint/issues/864#issuecomment-523213273)
         ecmaVersion: '2020',
         sourceType: 'module',

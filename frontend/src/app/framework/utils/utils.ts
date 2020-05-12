744f074a48459950e28761f3b86a1ffcaeeb4a5b
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-05-11 22:06:29 -0400

    (Continuation of last commit)

--- frontend/src/app/framework/utils/utils.ts
+++ frontend/src/app/framework/utils/utils.ts
@@ -1,15 +1,24 @@
-import CAPS from './utils/caps.enum';
-import changeCaps from './utils/change-caps.function';
-import checkAlphanumericity from './utils/check-alphanumericity.function';
-import forEach from './utils/for-each.function';
-import formatLocale from './utils/format-locale.function';
-import reinitType from './utils/reinit-type.function';
-import transferProperties from './utils/transfer-properties.function';
+import CAPS from './misc/caps.enum';
+
+//  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
+import changeCaps from './functions/change-caps.function';
+import checkAlphanumericity from './functions/check-alphanumericity.function';
+import forEach from './functions/for-each.function';
+import formatLocale from './functions/format-locale.function';
+import reinitType from './functions/reinit-type.function';
+import transferProperties from './functions/transfer-properties.function';
+
+//  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
+export {Utils};
 
 ////////////////////////////////////////////////////////////////////////////////
 /** General reusable utility code. */
-export const Utils = {
+const Utils = {
+
+    // Other
     CAPS,
+
+    // Functions
     changeCaps,
     checkAlphanumericity,
     forEach,

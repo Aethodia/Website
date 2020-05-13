7dd79915233ea345075b1afee512ef71eb8bd8c5
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-05-12 22:08:33 -0400

    Removed unused dependency from interceptors

--- frontend/src/app/framework/interceptors/cache.interceptor.ts
+++ frontend/src/app/framework/interceptors/cache.interceptor.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
+import {Observable} from 'rxjs';
 
 //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
 export {CacheInterceptor};

20a3d621d0c9ac1d3acb507edd9eb4b209c6060f
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-05-13 01:39:37 -0400

    The locale is now added to HttpHeaders

--- frontend/src/app/framework/interceptors/i18n.interceptor.ts
+++ frontend/src/app/framework/interceptors/i18n.interceptor.ts
@@ -3,15 +3,32 @@ import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/com
 import {Observable} from 'rxjs';
 
 //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
+import {I18nService} from '../services/i18n.service';
 export {I18nInterceptor};
 
 ////////////////////////////////////////////////////////////////////////////////
 @Injectable()
 class I18nInterceptor implements HttpInterceptor {
+
+    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
+    constructor(
+        private readonly i18nSvc: I18nService,
+    ) {
+        return this;
+    }
+
+    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
     intercept(
         request: HttpRequest<unknown>,
         next: HttpHandler,
     ): Observable<HttpEvent<unknown>> {
-        return next.handle(request); //TODO
+        const lang = this.i18nSvc.language.get().value;
+        if(lang != null) {
+            request.headers.set('Accept-Language', lang);
+            if(request.body != null) {
+                request.headers.set('Content-Language', lang);
+            }
+        }
+        return next.handle(request);
     }
 }

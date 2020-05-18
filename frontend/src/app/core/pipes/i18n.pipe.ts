b0d7cb1944153b693a91464a2444e05d051fff2d
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-05-18 02:18:21 -0400

    Localization works now.

--- frontend/src/app/core/pipes/i18n.pipe.ts
+++ frontend/src/app/core/pipes/i18n.pipe.ts
@@ -51,14 +51,39 @@ class I18nPipe implements PipeTransform {
     //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
     /** Looks up the translation associated with a key and locale.
      * @param key The key for the translation.
-     * @param lang The language to translate to.
-     * @returns the translation for the given key.
+     * @param lang The language to translate to.  Must be in the format of `xx` or `xx-XX`.
+     * @returns the translation for the given key, or the key itself if no translation is found.
      */
     private lookup(
         key: string,
         lang: string,
     ): string {
-        return key; //TODO
+
+        // Validate key
+        const parts = key.split('.');
+        if(parts.length > 2) throw new Error('`key` is not a valid `I18nKey`.');
+
+        // Validate i18nModule
+        if(this.bundle[parts[0]] === undefined) return key;
+        const i18nModule = this.bundle[parts[0]] as I18nModule;
+
+        // Validate i18nKey
+        if(!i18nModule[parts[1]] === undefined) return key;
+        const i18nKey = i18nModule[parts[1]] as I18nKey;
+
+        // Look up long-form locale
+        if(i18nKey[lang] !== undefined) return i18nKey[lang] as string;
+
+        // Look up short-form locale
+        lang = lang.split('-')[0];
+        if(i18nKey[lang] !== undefined) return i18nKey[lang] as string;
+
+        // Fall back to English
+        lang = 'en';
+        if(i18nKey[lang] !== undefined) return i18nKey[lang] as string;
+
+        // If all else fails, just return the key.
+        return key;
     }
 
     //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //

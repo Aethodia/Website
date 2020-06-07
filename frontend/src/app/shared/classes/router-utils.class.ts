c3695601c0dd321d54743205cb37b61a64cbcc09
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-06-07 19:46:48 -0400

    Lazy-loading is actually lazy now

--- frontend/src/app/shared/classes/router-utils.class.ts
+++ frontend/src/app/shared/classes/router-utils.class.ts
@@ -5,21 +5,21 @@ export {RouterUtils};
 class RouterUtils {
 
     //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
-    private static readonly modules: table<Promise<any>> = {
-        AdminModule: import('app/views/admin/admin.module'),
-        AuthModule:  import('app/views/auth/auth.module'),
-        HomeModule:  import('app/views/home/home.module'),
-        ErrorModule: import('app/views/error/error.module'),
+    private static readonly modules: table<() => Promise<any>> = {
+        AdminModule: () => import('app/views/admin/admin.module'),
+        AuthModule:  () => import('app/views/auth/auth.module'),
+        HomeModule:  () => import('app/views/home/home.module'),
+        ErrorModule: () => import('app/views/error/error.module'),
     };
 
     //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
-    public static loadChildren(moduleName: string) {
-        return async(): Promise<Class|void> => {
+    public static loadChildren<ModuleType>(moduleName: string) {
+        return async(): Promise<ModuleType|void> => {
             try {
                 if(RouterUtils.modules[moduleName] === undefined) {
                     throw ReferenceError(`Invalid \`moduleName\`: ${moduleName}`);
                 }
-                return (await RouterUtils.modules[moduleName])[moduleName];
+                return (await RouterUtils.modules[moduleName]!())[moduleName];
             } catch(error) {
                 console.error(error);
             }

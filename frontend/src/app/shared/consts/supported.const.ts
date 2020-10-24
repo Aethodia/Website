48023c001f37ac3615ee39a1925fa82785aa194e
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-10-23 20:41:17 -0400

    Added a list of supported languages

---
+++ frontend/src/app/shared/consts/supported.const.ts
@@ -0,0 +1,7 @@
+export {supportedLanguages}
+
+//  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
+/** A list of locales supported by the application. */
+const supportedLanguages = [
+    'en',
+];

8e1ae03c20507a6d44ae5d786eb7ac56d05c3db0
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-10-23 20:41:05 -0400

    Cleanup

--- frontend/src/app/core/consts/i18n.const.ts
+++ frontend/src/app/core/consts/i18n.const.ts
@@ -1,15 +1,19 @@
 import {InjectionToken, Provider} from '@angular/core';
 import {I18nBundle} from '../types/i18n.type';
+export {
+    I18N_BUNDLE,
+    setI18n,
+}
 
-////////////////////////////////////////////////////////////////////////////////
+//  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
 /** Provide this constant to configure the `I18nPipe`. */
-export const I18N_BUNDLE = new InjectionToken<I18nBundle>('I18N_BUNDLE');
+const I18N_BUNDLE = new InjectionToken<I18nBundle>('I18N_BUNDLE');
 
 //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
 /** Returns a provider for `I18N_BUNDLE`.
  * @param bundle The `I18nBundle` that `I18nPipe` should use.
  * @returns A provider for `I18N_BUNDLE` using the specified `I18nBundle`.
  */
-export function setI18n(bundle: I18nBundle): Provider {
+const setI18n = (bundle: I18nBundle): Provider => {
     return {provide: I18N_BUNDLE, useValue: bundle};
 }

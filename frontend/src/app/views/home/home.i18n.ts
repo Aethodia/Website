1d5851ede261c1393dd35b3f1e02d9bfb49dad2f
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-10-24 20:12:05 -0400

    Added a little text to the home page

--- frontend/src/app/views/home/home.i18n.ts
+++ frontend/src/app/views/home/home.i18n.ts
@@ -1,3 +1,4 @@
+import {LOREM_IPSUM} from 'app/core/consts/lorem-ipsum.const';
 import {I18nBundle} from 'app/core/types/i18n.type';
 export const HOME_I18N = {
     META_TITLE: {
@@ -9,8 +10,11 @@ export const HOME_I18N = {
     META_KEYWORDS: {
         en: `Theodia micronation technocracy`,
     },
-    WELCOME: {
+    INTRO_HEADER: {
         en: `Welcome!`,
+    },
+    INTRO_BODY: {
+        en: LOREM_IPSUM,
     },
 };
 export default HOME_I18N as I18nBundle; // This is a hack that ensures our bundle is the correct type without losing strictness.

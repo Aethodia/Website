aae0eea3ca5f83c87376d31e553c4b051b55d634
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-10-27 23:58:44 -0400

    Removed `marked`, due to limitations

--- frontend/src/app/core/consts/lorem-ipsum.const.ts
+++ frontend/src/app/core/consts/lorem-ipsum.const.ts
@@ -1,73 +1,74 @@
-import { Utils } from '../utils/utils';
 export {LOREM_IPSUM};
-const LOREM_IPSUM = Utils.md2html(`
-Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Nulla ut ipsum ut ex gravida aliquam.
-Ut quis nisl eu nulla rhoncus tincidunt.
-Integer vel lacus quis purus venenatis luctus.
-Cras convallis tristique mi ut interdum.
-Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-Donec lectus velit, convallis et justo vel, rutrum ornare diam.
-Vivamus auctor arcu mauris, et fringilla felis blandit vel.
-
-Mauris viverra odio odio, nec tincidunt orci fermentum nec.
-Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-Nullam feugiat augue ut sem volutpat, sed finibus enim imperdiet.
-Donec tristique, metus et fermentum facilisis, sapien odio molestie arcu, eget vestibulum metus enim et neque.
-Sed tempus odio vitae vulputate pretium.
-Fusce ornare ultrices ante, vel consectetur arcu tincidunt non.
-Pellentesque ullamcorper et lacus at aliquam.
-Duis tristique commodo purus, at convallis purus finibus a.
-Donec congue in purus ut pretium.
-Proin nec velit et erat eleifend sodales.
-Proin tincidunt vehicula orci, sed auctor turpis.
-Nullam et vestibulum eros.
-In hac habitasse platea dictumst.
-Integer tellus enim, cursus non feugiat quis, lacinia condimentum felis.
-
-Aliquam pulvinar, diam sit amet ultrices mollis, leo sapien consequat tortor, ac porta leo ipsum eu metus.
-Mauris blandit, dolor nec fringilla posuere, purus felis fermentum turpis, sit amet dictum felis magna at turpis.
-Morbi venenatis turpis id pretium congue.
-Morbi et purus eu erat tempus efficitur eu sit amet dolor.
-Vestibulum viverra sapien ac justo iaculis interdum.
-Nulla a molestie purus, quis vehicula enim.
-Aenean eget dapibus orci.
-Morbi varius id metus a vehicula.
-Pellentesque in felis eget dui vestibulum venenatis sed id nibh.
-Fusce at est iaculis, hendrerit risus at, hendrerit eros.
-Nulla leo purus, interdum ut vestibulum sodales, dapibus in enim.
-Pellentesque vel diam sed eros eleifend viverra sit amet id neque.
-Curabitur non orci vulputate velit ultrices eleifend.
-Vestibulum suscipit ipsum eu lectus porta, sed molestie tortor auctor.
-Nunc nec nulla efficitur, faucibus nulla vitae, hendrerit velit.
-Quisque fermentum sapien vel neque auctor lobortis.
-
-Cras sed scelerisque est, id hendrerit quam.
-Duis vel mauris magna.
-In congue malesuada dictum.
-Donec nulla purus, ultrices sed mauris sed, sagittis auctor nisi.
-Aenean ut lorem non lacus ultricies consectetur.
-Aliquam condimentum ac sapien ac molestie.
-Nunc erat felis, dignissim nec fringilla vel, aliquam malesuada arcu.
-Donec est sapien, fringilla in massa at, pulvinar gravida sem.
-Cras eu auctor lacus.
-Sed nec diam quam.
-Fusce efficitur orci at lorem luctus mattis.
-
-Duis efficitur quis massa eu venenatis.
-Aliquam feugiat convallis lectus, sit amet vestibulum dui ultrices non.
-Vivamus egestas ante a metus pharetra, sed fringilla lacus tempor.
-Donec bibendum eget sem et finibus.
-Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-Donec a vulputate lectus.
-Interdum et malesuada fames ac ante ipsum primis in faucibus.
-Mauris consequat aliquam leo, sit amet iaculis augue eleifend non.
-Nulla nec vehicula mi.
-In accumsan porttitor nunc ac ultrices.
-In hac habitasse platea dictumst.
-Sed velit ligula, vulputate eu odio eget, consequat auctor odio.
-Nam mollis nisl a dui aliquet, non elementum elit sodales.
-Integer aliquet orci at augue eleifend iaculis vel sed sapien.
-Nullam sodales ante nec sapien accumsan tristique.
-In mollis risus quis justo scelerisque gravida a ut velit.
-`);
+const LOREM_IPSUM = `
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>
+        Nulla ut ipsum ut ex gravida aliquam.<br/>
+        Ut quis nisl eu nulla rhoncus tincidunt.<br/>
+        Integer vel lacus quis purus venenatis luctus.<br/>
+        Cras convallis tristique mi ut interdum.<br/>
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.<br/>
+        Donec lectus velit, convallis et justo vel, rutrum ornare diam.<br/>
+        Vivamus auctor arcu mauris, et fringilla felis blandit vel.<br/>
+    </p><p>
+        Mauris viverra odio odio, nec tincidunt orci fermentum nec.<br/>
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.<br/>
+        Nullam feugiat augue ut sem volutpat, sed finibus enim imperdiet.<br/>
+        Donec tristique, metus et fermentum facilisis, sapien odio molestie arcu, eget vestibulum metus enim et neque.<br/>
+        Sed tempus odio vitae vulputate pretium.<br/>
+        Fusce ornare ultrices ante, vel consectetur arcu tincidunt non.<br/>
+        Pellentesque ullamcorper et lacus at aliquam.<br/>
+        Duis tristique commodo purus, at convallis purus finibus a.<br/>
+        Donec congue in purus ut pretium.<br/>
+        Proin nec velit et erat eleifend sodales.<br/>
+        Proin tincidunt vehicula orci, sed auctor turpis.<br/>
+        Nullam et vestibulum eros.<br/>
+        In hac habitasse platea dictumst.<br/>
+        Integer tellus enim, cursus non feugiat quis, lacinia condimentum felis.<br/>
+    </p><p>
+        Aliquam pulvinar, diam sit amet ultrices mollis, leo sapien consequat tortor, ac porta leo ipsum eu metus.<br/>
+        Mauris blandit, dolor nec fringilla posuere, purus felis fermentum turpis, sit amet dictum felis magna at turpis.<br/>
+        Morbi venenatis turpis id pretium congue.<br/>
+        Morbi et purus eu erat tempus efficitur eu sit amet dolor.<br/>
+        Vestibulum viverra sapien ac justo iaculis interdum.<br/>
+        Nulla a molestie purus, quis vehicula enim.<br/>
+        Aenean eget dapibus orci.<br/>
+        Morbi varius id metus a vehicula.<br/>
+        Pellentesque in felis eget dui vestibulum venenatis sed id nibh.<br/>
+        Fusce at est iaculis, hendrerit risus at, hendrerit eros.<br/>
+        Nulla leo purus, interdum ut vestibulum sodales, dapibus in enim.<br/>
+        Pellentesque vel diam sed eros eleifend viverra sit amet id neque.<br/>
+        Curabitur non orci vulputate velit ultrices eleifend.<br/>
+        Vestibulum suscipit ipsum eu lectus porta, sed molestie tortor auctor.<br/>
+        Nunc nec nulla efficitur, faucibus nulla vitae, hendrerit velit.<br/>
+        Quisque fermentum sapien vel neque auctor lobortis.<br/>
+    </p><p>
+        Cras sed scelerisque est, id hendrerit quam.<br/>
+        Duis vel mauris magna.<br/>
+        In congue malesuada dictum.<br/>
+        Donec nulla purus, ultrices sed mauris sed, sagittis auctor nisi.<br/>
+        Aenean ut lorem non lacus ultricies consectetur.<br/>
+        Aliquam condimentum ac sapien ac molestie.<br/>
+        Nunc erat felis, dignissim nec fringilla vel, aliquam malesuada arcu.<br/>
+        Donec est sapien, fringilla in massa at, pulvinar gravida sem.<br/>
+        Cras eu auctor lacus.<br/>
+        Sed nec diam quam.<br/>
+        Fusce efficitur orci at lorem luctus mattis.<br/>
+    </p><p>
+        Duis efficitur quis massa eu venenatis.<br/>
+        Aliquam feugiat convallis lectus, sit amet vestibulum dui ultrices non.<br/>
+        Vivamus egestas ante a metus pharetra, sed fringilla lacus tempor.<br/>
+        Donec bibendum eget sem et finibus.<br/>
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.<br/>
+        Donec a vulputate lectus.<br/>
+        Interdum et malesuada fames ac ante ipsum primis in faucibus.<br/>
+        Mauris consequat aliquam leo, sit amet iaculis augue eleifend non.<br/>
+        Nulla nec vehicula mi.<br/>
+        In accumsan porttitor nunc ac ultrices.<br/>
+        In hac habitasse platea dictumst.<br/>
+        Sed velit ligula, vulputate eu odio eget, consequat auctor odio.<br/>
+        Nam mollis nisl a dui aliquet, non elementum elit sodales.<br/>
+        Integer aliquet orci at augue eleifend iaculis vel sed sapien.<br/>
+        Nullam sodales ante nec sapien accumsan tristique.<br/>
+        In mollis risus quis justo scelerisque gravida a ut velit.<br/>
+    </p>
+`;
--- frontend/src/app/views/root/root.i18n.ts
+++ frontend/src/app/views/root/root.i18n.ts
@@ -1,8 +1,5 @@
-import {Utils} from 'app/core/utils/utils';
 import {I18nBundle} from 'app/core/types/i18n.type';
 export {ROOT_I18N}
-
-////////////////////////////////////////////////////////////////////////////////
 const ROOT_I18N = {
     ACCOUNT_NAV_BUTTON: {
         en: `Account`,
@@ -20,7 +17,7 @@ const ROOT_I18N = {
         en: `Services`,
     },
     COPYRIGHT_NOTICE: {
-        en: Utils.md2html(`Copyright ©:  per [CC-BY-SA 4](https://creativecommons.org/licenses/by-sa/4.0);  from 2018-2020;  by Miles B Huff;  on behalf of the Technocratic Republic of Theodia.`),
+        en: `Copyright ©:  per <a href="https://creativecommons.org/licenses/by-sa/4.0">CC-BY-SA 4</a>;  from 2018-2020;  by Miles B Huff;  on behalf of the Technocratic Republic of Theodia.`,
     },
 };
 export default ROOT_I18N as I18nBundle; // This is a hack that ensures our bundle is the correct type without losing strictness.
